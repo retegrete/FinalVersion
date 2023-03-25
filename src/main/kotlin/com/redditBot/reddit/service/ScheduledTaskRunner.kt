e96099fd2432d5cf5fb38d95e144a74aeaf39199
Mario Ivan <ortunoitzmario@gmail.com>
2023-03-25 17:35:17 -0600

    Update ScheduledTaskRunner.kt

--- src/main/kotlin/com/redditBot/reddit/service/ScheduledTaskRunner.kt
+++ src/main/kotlin/com/redditBot/reddit/service/ScheduledTaskRunner.kt
@@ -25,7 +25,7 @@ class ScheduledTaskRunner(
 
     @Value("\${twilio.to}")
     private lateinit var toNumber: String
-    @Scheduled(cron = "0 36 22 * * ?", zone = "America/Chicago")
+    @Scheduled(cron = "0 38 23 * * ?", zone = "America/Chicago")
     fun startScheduledTasks() {
         val toPhoneNumber = "whatsapp:$toNumber" // Replace with the destination phone number
         val fromPhoneNumber = "whatsapp:$fromNumber" // Replace with your Twilio phone number
